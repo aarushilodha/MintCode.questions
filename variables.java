deedaf88136fe15f0e1fe31c88c15028770bfe57
aarushilodha <aarushilodha20@gmail.com>
2024-11-23 11:14:26 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,12 @@
+public class variables {
+    public static void main(String[] args) {
+        String name = "Aman";
+        int age = 21;
+        String neighbour = "Aaru";
+        String friend = neighbour;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Neighbour: "+ neighbour);
+        System.out.println("Friend: "+ friend);
+    }
+}
