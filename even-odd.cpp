cbf0dea18ee5107cf737ce3b23d07def429ecc13
aarushilodha <aarushilodha20@gmail.com>
2024-11-24 16:47:11 +0530

    Add files via upload

---
+++ even-odd.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Even_odd{
+public String Even_Odd(int n){
+    Even_Odd(n);
+    if(n%2==0)
+    {
+        return "Even";
+    }
+    else{
+       return "Odd";
+    }
+
+}
+}
+int main()
+{
+    int n;
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+    return 0;
+}
